2ac63d2c25fb6a291dac2202879a44f752523798
yudhahendrianto <yudhahendrianto@apps.ipb.ac.id>
2023-03-16 09:10:03 +0700

    Commit navbar

--- projects/base-area/src/app/components/navbar/navbar.module.ts
+++ projects/base-area/src/app/components/navbar/navbar.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { NavbarComponent } from './navbar.component';
 import { ShareModule } from '../../common/share.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({ 
     declarations: [
         NavbarComponent
       ],
       imports: [
-        ShareModule
+        ShareModule,
+        BrowserAnimationsModule
       ],
       exports : [
         NavbarComponent
