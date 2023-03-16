81c840cb948b41da4e36a0a2bf8d1bb36191d43e
eronwahyu <wahyueron@gmail.com>
2023-03-16 11:46:39 +0700

    Update MyActivity Eron

--- projects/member-area/src/app/pages/my-activity/my-event/my-event.component.ts
+++ projects/member-area/src/app/pages/my-activity/my-event/my-event.component.ts
@@ -1,6 +1,14 @@
 import { Component, OnInit } from "@angular/core";
+import { FormBuilder } from "@angular/forms";
 import { Title } from "@angular/platform-browser";
 import { ActivatedRoute, Router } from "@angular/router";
+import { ActivityRes } from "@dto/activity/activity-res";
+import { CategoryRes } from "@dto/category/category-res";
+import { ActivityService } from "@service/activity.service";
+import { CategoryService } from "@service/category.service";
+import { ACTIVITY_CODE } from "projects/base-area/src/app/constant/activity-code";
+import { ACTIVITY_LIMIT } from "projects/base-area/src/app/constant/activity-limit";
+import { ACTIVITY_TYPE } from "projects/base-area/src/app/constant/activity-type";
 import { Subscription } from "rxjs";
 
 @Component({
@@ -45,9 +53,78 @@ import { Subscription } from "rxjs";
 
 })
 export class MyEventComponent {
+    private event$? : Subscription
+    eventList? : ActivityRes[]
+    private category$? : Subscription
+    categoryList? : CategoryRes[]
+    private eventCreated$? : Subscription
+    eventListCreated? : ActivityRes[]
+    private categoryCreated$? : Subscription
+    categoryListCreated? : CategoryRes[]
+    page = 1
+    categories=this.fb.group({
+        category:[[]],
+
+    })
+
+    categoriesCreated=this.fb.group({
+        categoryCreated:[[]],
+
+    })
     constructor(
-        private router: Router
+        private router: Router,
+        private activityService: ActivityService,
+        private categoryService:CategoryService,
+        private fb:FormBuilder
     ) { }
+    ngOnInit(): void {
+        this.event$ = this.activityService.getActivityByType(ACTIVITY_LIMIT, this.page,ACTIVITY_TYPE.EV,undefined,ACTIVITY_CODE.PURCHASE).subscribe(result => {
+            
+            this.eventList = result
+        })
+        this.category$ = this.categoryService.getCategory().subscribe(result => {
+            this.categoryList = result
+        })
+        this.eventCreated$ = this.activityService.getActivityByType(ACTIVITY_LIMIT, this.page,ACTIVITY_TYPE.EV,undefined,ACTIVITY_CODE.PROFILE).subscribe(result => {
+            
+            
+            this.eventListCreated = result
+        })
+        this.categoryCreated$ = this.categoryService.getCategory().subscribe(result => {
+            this.categoryListCreated = result
+        })
+
+        this.categories.get('category')?.valueChanges.subscribe(result => {
+            const temp=result as any;
+            console.log(result);
+            if(!temp.length){
+                
+                this.event$ = this.activityService.getActivityByType(ACTIVITY_LIMIT, this.page,ACTIVITY_TYPE.EV,undefined,ACTIVITY_CODE.PURCHASE).subscribe(result => {
+            
+                    this.eventList = result
+                })
+            }else{
+                this.event$ = this.activityService.getActivityByType(ACTIVITY_LIMIT, this.page,ACTIVITY_TYPE.EV,temp,ACTIVITY_CODE.PURCHASE).subscribe(result => {
+                    this.eventList = result
+                })
+            }
+        })
+        this.categoriesCreated.get('categoryCreated')?.valueChanges.subscribe(result => {
+            const temp=result as any;
+            if(!temp.length){
+                
+                this.eventCreated$ = this.activityService.getActivityByType(ACTIVITY_LIMIT, this.page,ACTIVITY_TYPE.EV,undefined,ACTIVITY_CODE.PROFILE).subscribe(result => {
+            
+                    this.eventListCreated = result
+                })
+            }else{
+                this.eventCreated$ = this.activityService.getActivityByType(ACTIVITY_LIMIT, this.page,ACTIVITY_TYPE.EV,temp,ACTIVITY_CODE.PROFILE).subscribe(result => {
+                    this.eventListCreated = result
+                })
+            }
+        })
+     
+    }
 
     onCreatePost() {
         this.router.navigateByUrl('/posts/create')
