b814c6d26c3cd08419870d30d99d3d6d17eca2d7
astutiannisa <39583549+astutiannisa@users.noreply.github.com>
2023-03-31 13:15:22 +0700

    post profile

--- projects/base-area/src/app/components/navbar/navbar.component.ts
+++ projects/base-area/src/app/components/navbar/navbar.component.ts
@@ -35,6 +35,7 @@ export class NavbarComponent {
   src!: string
   fileId!: string
   res!: string
+
   constructor(
     private router: Router,
     private activityService: ActivityService,
@@ -46,7 +47,7 @@ export class NavbarComponent {
 
   adminProfile: MenuItem[] = [
     {
-      label: 'Hello, Admin!',
+      label: `Hello, ${this.userService.user.fullName}!`,
 
     },
     {
@@ -70,7 +71,7 @@ export class NavbarComponent {
 
   memberProfile: MenuItem[] = [
     {
-      label: 'Hello, User!',
+      label: `Hello, ${this.userService.user.fullName}!`,
 
     },
     {
@@ -204,14 +205,12 @@ export class NavbarComponent {
 
   ngOnInit(): void {
 
-
       if (this.userService.user.fileId) {
         this.src = `${BASE_URL}/files/${this.userService.user.fileId!}`
       } else {
         this.src = this.userService.user.fileBase64
         this.res = this.userService.user.fileBase64
       }
-
 
       if (this.roleCode == Role.SuperAdmin) {
         this.items = this.superAdminNav
@@ -220,7 +219,6 @@ export class NavbarComponent {
       else if (this.roleCode == Role.Member) {
         this.items = this.memberNav
         this.itemProfile = this.memberProfile
-
       }
 
       else if (this.roleCode == Role.Admin) {
@@ -228,13 +226,10 @@ export class NavbarComponent {
         this.itemProfile = this.adminProfile
       }
 
-
       this.activityType$ = this.activityService.getActivityType().subscribe(result => {
         this.activityTypeList = result
       })
 
-
-    
   }
 
   onLogout() {
